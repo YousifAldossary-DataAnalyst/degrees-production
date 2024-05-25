d1fd9c7768433a6a67b39822ff7fb3933807c425
Yousif Aldossary <86018992+YousifAldossary-DataAnalyst@users.noreply.github.com>
2024-05-25 20:24:47 +0300

    Automation production2

--- src/app/(main)/subaccount/[subaccountId]/workflows/[workflowId]/_actions/workflow-connections.tsx
+++ src/app/(main)/subaccount/[subaccountId]/workflows/[workflowId]/_actions/workflow-connections.tsx
@@ -83,7 +83,7 @@ export const onCreateNodeTemplate = async (
 
       if (channelList) {
         //remove duplicates before insert
-        const NonDuplicated = channelList.slackChannels?.filter(
+        const NonDuplicated = channelList.slackChannels.filter(
           (channel) => channel !== channels![0].value
         );
 
