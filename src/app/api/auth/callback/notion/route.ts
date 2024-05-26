3db4ec0b34f45fcd49ce22f6a76a978616368ded
Yousif Aldossary <86018992+YousifAldossary-DataAnalyst@users.noreply.github.com>
2024-05-26 14:08:44 +0300

    Automation production15Test13

--- src/app/api/auth/callback/notion/route.ts
+++ src/app/api/auth/callback/notion/route.ts
@@ -7,7 +7,7 @@ export async function GET(req: NextRequest) {
   const code = req.nextUrl.searchParams.get("code");
 
   let subaccountId = ''
-  
+
   console.log(subaccountId);
 
   const subaccount = await db.subAccount.findUnique({
@@ -55,11 +55,11 @@ export async function GET(req: NextRequest) {
       //WIP: Add subaccount path to connections to get the api to redirect back.
 
       return NextResponse.redirect(
-        `${process.env.NEXT_PUBLIC_URL}/subaccount/${subaccount?.id}/connections?access_token=${response.data.access_token}&workspace_name=${response.data.workspace_name}&workspace_icon=${response.data.workspace_icon}&workspace_id=${response.data.workspace_id}&database_id=${databaseId}`
+        `${process.env.NEXT_PUBLIC_URL}/subaccount/${subaccountId}/connections?access_token=${response.data.access_token}&workspace_name=${response.data.workspace_name}&workspace_icon=${response.data.workspace_icon}&workspace_id=${response.data.workspace_id}&database_id=${databaseId}`
       );
     }
   }
   return NextResponse.redirect(
-    `${process.env.NEXT_PUBLIC_URL}/subaccount/${subaccount?.id}/connections`
+    `${process.env.NEXT_PUBLIC_URL}/subaccount/${subaccountId}/connections`
   );
 }
